246be37263cabaa0da1189c3a1b39bb861aee894
Ramy <Ramy@Ramy-Ashour>
2018-08-22 16:18:12 +0200

    zuul server

--- src/main/java/com/joi/api/eureka/ZuulServerApplication.java
+++ src/main/java/com/joi/api/eureka/ZuulServerApplication.java
@@ -2,16 +2,16 @@ package com.joi.api.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 //API Gatway (Zuul) + Service Registery (Eureka)
 @EnableZuulProxy
-@EnableEurekaServer
+@EnableDiscoveryClient
 @SpringBootApplication
-public class EurekaServiceApplication {
+public class ZuulServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServiceApplication.class, args);
+        SpringApplication.run(ZuulServerApplication.class, args);
     }
 }
